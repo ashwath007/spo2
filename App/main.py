b27ee7e54b9dabb6df4c936d0370548071fdce57
vicky <srivicky2000@gmail.com>
2021-01-04 12:34:13 +0530

    SPO2 kivy2

--- App/main.py
+++ App/main.py
@@ -6,8 +6,13 @@ class Spo2App(App):
     def build(self):
         return Button(
             text='I am DJV',
-            pos=(50,50)
+            pos=(50,50),
+            size=(100,50),
+            size_hint=(None,None)
             )
+
+
+
 
 
 if __name__ == '__main__':
