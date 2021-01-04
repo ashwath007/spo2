6bc40bb32ff1945916dcdf9eaf7f375f8fd71d37
vicky <srivicky2000@gmail.com>
2021-01-04 12:37:06 +0530

    SPO2 kivy3

--- App/main.py
+++ App/main.py
@@ -2,14 +2,16 @@ from kivy.app import App
 from kivy.uix.button import Button
 
 
+class AddButton(Button):
+    pass 
+
+
 class Spo2App(App):
     def build(self):
-        return Button(
-            text='I am DJV',
-            pos=(50,50),
-            size=(100,50),
-            size_hint=(None,None)
-            )
+        return AddButton(
+            text="Add",
+            pos=(100,100),
+        )
 
 
 
