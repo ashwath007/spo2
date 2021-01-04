9dbb9b75266732a8981adc7709680af6f6cfe42e
vicky <srivicky2000@gmail.com>
2021-01-04 12:28:00 +0530

    SPO2 kivy1

--- App/main.py
+++ App/main.py
@@ -4,7 +4,10 @@ from kivy.uix.button import Button
 
 class Spo2App(App):
     def build(self):
-        return Button(text='I am DJV')
+        return Button(
+            text='I am DJV',
+            pos=(50,50)
+            )
 
 
 if __name__ == '__main__':
